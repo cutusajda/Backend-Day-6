9e7eee839bab6bc35500a3ee3bb3bec6dd655a29
Sajda Parveen <sajdak486@gmail.com>
2026-02-04 12:00:48 +0530

    practicing

--- src/app.js
+++ src/app.js
@@ -12,17 +12,103 @@ app.use(express.json())
 // post /notes
 // req.body =>{title,description}
 
-app.post("/notes", async(req,res)=>{
-    const{ title, description} = req.body
 
-   const note = await noteModel.create({
+// app.post("/notes", async(req,res)=>{
+//     const{ title, description} = req.body
+
+//    const note = await noteModel.create({
+//         title,description
+//     })
+
+//     res.status(201).json({
+//         message:"Note Created Sucessfully ",
+//         note
+//     })
+// })
+
+// GET /notes
+// fetch all the notes data
+app.get("/notes",async (req,res)=>{
+    //find method hamesha array of objects mea he data return krea gi
+    const notes = await noteModel.find()
+
+    res.status(200).json({
+        message:"notes data fetch sucessfully",
+        notes
+    })
+});
+
+// delete
+// app.delete("/notes",(req,res)=>{
+//     message:"notes deleted sucessfully",
+//     notes
+
+// })
+
+
+
+// post/api/notes
+// create new note and save data in mongodb
+// req.body={title , description}
+
+    app.post('/api/notes', async (req,res)=>{
+    const{title,description} = req.body
+    
+     const note = await noteModel.create({
         title,description
     })
 
     res.status(201).json({
-        message:"Note Created Sucessfully ",
-        note
+        message:"note created sucessfully"
     })
 })
+
+
+// GET /api/notes
+// Fetch all the notes data from mongodb and send them in the response
+app.get("/api/notes",async (req,res)=>{
+    const notes = await noteModel.find()
+
+    res.status(200).json({
+        message:"notes fetched sucessfully",
+        notes
+    })
+
+})
+
+
+// Delete /api/notes/:id
+// delete note with the id from req.params
+
+app.delete("/api/notes/:id",async (req,res)=>{
+    const id = req.params.id;
+    console.log(id);
+
+    await noteModel.findByIdAndDelete(id)
+
+    res.status(200).json({
+        message:"note deleted sucessfully"
+        
+    })
+
+})
+
+
+// PATCH /api/notes/:id
+// update the description of the note by id
+// req.body = {description}
+app.patch("/api/notes/:id",async (req,res)=>{
+    const id = req.params.id;
+    const { description } = req.body
+
+    await noteModel.findByIdAndUpdate(id,{ description })
+
+    res.status(200).json({
+        message:"note updated sucessfully ",
+    })
+
+
+})
+
 
 module.exports = app
